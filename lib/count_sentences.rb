0a22e2bdd79eac017776c6b8b7e26b500e25cfd2
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-14 01:05:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,8 +3,8 @@ require 'pry'
 class String
 
   def sentence?
-    self = self.split[" "]
-    true if(self[self.length-1].include?("."))
+    array = self.split[" "]
+    true if(array[array.length-1].include?("."))
   end
 
   def question?
