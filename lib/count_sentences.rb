5ef218a98ddd4914387f8bd837e7c487ea834003
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-14 01:08:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,8 +5,7 @@ class String
   @array = self.split(" ")
 
   def sentence?
-
-    if(array[(array.length)-1].include?("."))
+    if(@array[(@array.length)-1].include?("."))
       true
     else
       false
